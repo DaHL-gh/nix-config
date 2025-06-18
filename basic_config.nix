f00f91c7daa56d0eb286d6fd3bea2f6bb25f33f8
DaHL <8tima18@gmail.com>
2025-06-19 00:20:57 +0500

    add hyprland config

--- basic_config.nix
+++ basic_config.nix
@@ -72,8 +72,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    kitty
-
     clang
     spotify
     telegram-desktop
