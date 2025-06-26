b9c3e6627f82722675fd2201300103bda6916313
DaHL <8tima18@gmail.com>
2025-06-26 17:09:20 +0500

    nvim: disable plugins update notification

--- home/modules/neovim/src/lua/dahl/lazy.lua
+++ home/modules/neovim/src/lua/dahl/lazy.lua
@@ -30,5 +30,5 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
 
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
