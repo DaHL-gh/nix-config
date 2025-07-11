847fbd1b3090804f5f7c52010b00af443499b2e2
DaHL <8tima18@gmail.com>
2025-07-11 23:36:58 +0500

    neovim: change default theme

--- home/modules/neovim/src/lua/dahl/themes.lua
+++ home/modules/neovim/src/lua/dahl/themes.lua
@@ -1,4 +1,11 @@
 return {
 	{ "blazkowolf/gruber-darker.nvim" },
 	{ "ficcdaf/ashen.nvim" },
+	{
+		"navarasu/onedark.nvim",
+		opts = {
+			style = "darker",
+			toggle_style_key = "<leader>ts"
+		}
+	},
 }
