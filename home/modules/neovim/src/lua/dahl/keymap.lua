64d1ab232875eaa74a1d1a752f8693e0580064f6
DaHL <8tima18@gmail.com>
2025-07-11 23:37:24 +0500

    neovim: change keymap

--- home/modules/neovim/src/lua/dahl/keymap.lua
+++ home/modules/neovim/src/lua/dahl/keymap.lua
@@ -1,34 +1,33 @@
-local opts = { noremap = true, silent = true }
-local map = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
 
--- map("n", "<leader>s")
+local function map_cmd(mode, binding, command, opts)
+	opts = opts or default_opts
+	vim.keymap.set(mode, binding, ":" .. command .. "<CR>", opts)
+end
+
+local function map(mode, binding, func, args, opts)
+	opts = opts or default_opts
+	if args then
+		vim.keymap.set(mode, binding, function() func(args) end, opts)
+	else
+		vim.keymap.set(mode, binding, func, opts)
+	end
+end
+
 
 -- NeoTree
-map("n", "<leader><Tab>", ":Neotree toggle <CR>", opts)
+map_cmd("n", "<leader><Tab>", "Neotree toggle")
+
+-- Lazy
+map_cmd("n", "<leader>L", "Lazy")
 
 -- LSP
-map("n", "<leader>f", vim.lsp.buf.format)
-map("n", "<leader>r", vim.lsp.buf.rename)
-map("n", "<leader>a", vim.lsp.buf.code_action)
+map("n", "<leader>lf", vim.lsp.buf.format)
+map("n", "<leader>lr", vim.lsp.buf.rename)
+map("n", "<leader>la", vim.lsp.buf.code_action)
+map("n", "<leader>ld", vim.diagnostic.open_float)
 
--- Device	S-state	 Status   Sysfs node
--- GP12	 S4	*enabled   pci:0000:00:07.1
--- GP13	 S4	*enabled   pci:0000:00:08.1
--- XHC0	 S4	*disabled  pci:0000:09:00.3
--- GP30	 S4	*disabled
--- GP31	 S4	*disabled
--- PS2K	 S3	*disabled
--- GPP0	 S4	*enabled   pci:0000:00:01.1
--- GPP8	 S4	*enabled   pci:0000:00:03.1
--- SWUS	 S4	*enabled   pci:0000:05:00.0
--- SWDS	 S4	*enabled   pci:0000:06:00.0
--- PTXH	 S4	*enabled   pci:0000:02:00.0
--- PT20	 S4	*disabled
--- PT21	 S4	*disabled
--- PT22	 S4	*disabled
--- PT23	 S4	*disabled
--- PT24	 S4	*disabled
--- PT26	 S4	*disabled
--- PT27	 S4	*disabled
--- PT28	 S4	*disabled
--- PT29	 S4	*enabled   pci:0000:03:09.0
+map("n", "<leader>ln", vim.diagnostic.jump, { count = 1, float = true })
+map("n", "<leader>lp", vim.diagnostic.jump, { count = -1, float = true })
+
+-- GIT
