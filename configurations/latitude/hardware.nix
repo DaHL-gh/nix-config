1fc49f56c065695f62c1de7bb23035fa0e672761
DaHL <8tima18@gmail.com>
2025-07-16 11:37:29 +0500

    add swapfile and hybernation to latitude conf

--- configurations/latitude/hardware.nix
+++ configurations/latitude/hardware.nix
@@ -3,10 +3,20 @@
 {
 	imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-	boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-	boot.initrd.kernelModules = [ ];
-	boot.kernelModules = [ "kvm-intel" ];
-	boot.extraModulePackages = [ ];
+	boot = {
+		initrd = {
+			availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+			kernelModules = [ ];
+		};
+		kernelModules = [ "kvm-intel" ];
+		extraModulePackages = [ ];
+
+		resumeDevice = "/dev/sda7";
+		kernelParams = [
+			"resume=/dev/sda7"
+			"resume_offset=37263360"
+		];
+	};
 
 	fileSystems."/" = { 
 		device = "/dev/sda7";
@@ -30,7 +40,10 @@
 		options = [ "defaults" "nofail" ];
 	};
 
-	swapDevices = [ ];
+	swapDevices = [{
+		device = "/swapfile";
+		size = 8192;
+	}];
 
 	# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
 	# (the default) this is the recommended approach. When using systemd-networkd it's
