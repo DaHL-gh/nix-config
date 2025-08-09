edf75b34ecacf51bceda6b5df25eea18556b6730
DaHL <8tima18@gmail.com>
2025-08-09 05:13:58 +0500

    update ntfs mountning params

--- configurations/latitude/hardware.nix
+++ configurations/latitude/hardware.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, modulesPath, ... }:
 let
 	basic_mounting_params = ["defaults" "nofail"];
-	fs_mask = ["dmask=027" "fmask=137"];
+	fs_mask = ["dmask=027" "fmask=027"];
 	user_mask = ["uid=1000" "gid=1000"];
 
 	ext4_mounting_params = basic_mounting_params;
