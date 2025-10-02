2949452f1ff6fb3a2817902a7b2711f0816b48a8
DaHL <8tima18@gmail.com>
2025-10-02 17:56:04 +0500

    neovim: add diffview, update keymaps, update options

--- home/modules/neovim/src/lua/dahl/options.lua
+++ home/modules/neovim/src/lua/dahl/options.lua
@@ -1,7 +1,14 @@
+vim.opt.winborder = "rounded"
+vim.opt.tabstop = 4
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
 
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.fillchars:append { diff = "╱" }
