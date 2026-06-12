c96a5378a19db035003ca33cd7401d61c0cd6890
dahl <8tima18@gmail.com>
2026-06-13 02:04:34 +0500

    git: enable lfs

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -10,6 +10,7 @@
   config = lib.mkIf config.localModules.git.enable {
     programs.git = {
       enable = true;
+      lfs.enable = true;
 
       settings = {
         init.defaultBranch = "main";
